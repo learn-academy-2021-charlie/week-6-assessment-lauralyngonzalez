c103e17090f5b2e6bf0080c844f6460b637bf227
lauralyngonzalez <lauralyn.gonzalez@gmail.com>
2021-08-22 12:45:15 -0700

    rails commenting

--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) Controller to retrieve data from the BlogPost model. Utilizes the REST API principles
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) Index uses Active Record to return a collection of all blog posts and saves it to an instance variable, @posts.
     @posts = BlogPost.all
   end
 
   def show
-    # ---3)
+    # ---3) Active Record queries the database to find the BlogPost with the id given in params.
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) Instantiates a new Post object and saves it to @post instance variable.
   def new
     @post = Post.new
   end
 
   def create
-    # ---5)
+    # ---5) Creates and saves a new record in the db. blog_post_params is called to require and permit the title and content attributes. If @post is valid, then the page redirects to the new blog post, otherwise it stays on the create page.
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -33,14 +33,14 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  # ---6)
+  # ---6) Returns the blog post with the matching primary key
   def edit
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) The Active Record object was retrieved and its attributes are updated. If the object is valid then the page redirects to the updated post, otherwise it stays on the edit page.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8) Redirects back to the blog post if the post was not destroyed.
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) Private methods used for this class. Methods defined below private are only accessible from within BlogPostsController
   private
   def blog_post_params
-    # ---10)
+    # ---10) Allows us to choose the title and content attributes to update in the db and permits them being exposed. blog_post parameters are required.
     params.require(:blog_post).permit(:title, :content)
   end
 end
